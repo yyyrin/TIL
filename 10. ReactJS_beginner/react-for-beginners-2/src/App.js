689e77f33833df0c12b791d88cd64e03f75aaa20
yyyrin <qsc130@gmail.com>
2023-03-13 03:23:30 +0900

    ing

--- 10. ReactJS_beginner/react-for-beginners-2/src/App.js
+++ 10. ReactJS_beginner/react-for-beginners-2/src/App.js
@@ -1,5 +1,14 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  // input 값을 어떻게 control할 수 있을까? -> useState 이용
+  const [toDo, setToDo] = useState("");
+
+  return (
+    <div>
+      <input value={toDo} type="text" placeholder="Write your to do..." />
+    </div>
+  );
 }
 
 export default App;
